b587f7f39006920bbc79e4bd75de6cae57917be9
Viktor Seredovych <vseredov@cisco.com>
2020-12-22 21:34:12 +0200

    Fix

--- templates/bootstrap.sh
+++ templates/bootstrap.sh
@@ -10,7 +10,4 @@ SYSTEMD_SERVICE_NAME="minecraft"
 # Mount volume, add crontab for backups and start minecraft
 # -------–––––––––––––––––––––––––––––––––––––––––––––––––––––––
 mount /dev/disk/by-id/${GCP_PERSISTANT_VOLUME_NAME} ${MINECRAFT_SERVER_HOME}
-
-crontab -l | grep -v -F "${MINECRAFT_SERVER_HOME}/backup.sh" ; echo "*/30 * * * * ${MINECRAFT_SERVER_HOME}/backup.sh") | crontab -
-
 sudo service ${SYSTEMD_SERVICE_NAME} start
