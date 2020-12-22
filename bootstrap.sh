13ce474575fe35fcd2eb6c4ac7654e20461a0747
Viktor <44211135+vseredovych@users.noreply.github.com>
2020-12-22 22:30:04 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,7 @@
 # -------–––––––––––––––––––––––––––––––––––––––––––––––––––––––
 GCP_PERSISTANT_VOLUME_NAME="google-minecraft-disk"
 SYSTEMD_SERVICE_NAME="minecraft"
+MINECRAFT_SERVER_HOME="/home/minecraft"
 
 # -------–––––––––––––––––––––––––––––––––––––––––––––––––––––––
 # Mount volume, add crontab for backups and start minecraft
