0ea0ccc34e4bac0f41bf5397f8a3200ab3ddf597
Viktor <44211135+vseredovych@users.noreply.github.com>
2020-12-24 20:01:01 +0200

    Update shutdown.sh

--- shutdown.sh
+++ shutdown.sh
@@ -8,5 +8,5 @@ SYSTEMD_SERVICE_NAME="minecraft"
 # -------–––––––––––––––––––––––––––––––––––––––––––––––––––––––
 # Backup server and stop
 # -------–––––––––––––––––––––––––––––––––––––––––––––––––––––––
-./BACKUP_SCRIPT
+sudo ${BACKUP_SCRIPT}
 sudo service ${SYSTEMD_SERVICE_NAME} stop
